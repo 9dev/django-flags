75647193696a81375c7abf799c37faeb91471572
9dev <9devmail@gmail.com>
2016-03-15 19:52:11 +0100

    Created flag model.

--- flags/models.py
+++ flags/models.py
@@ -1,3 +1,12 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
-# Create your models here.
+
+class Flag(models.Model):
+    object_id = models.PositiveIntegerField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    creator = models.ForeignKey('auth.User')
+    creation_date = models.DateTimeField(auto_now_add=True)
