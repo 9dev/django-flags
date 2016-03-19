2064bb161186a41a17a9ffe8893a267241a6e47d
9dev <9devmail@gmail.com>
2016-03-19 14:01:17 +0100

    Added flags urls to main urlconf.

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -3,11 +3,14 @@ from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 
+from flags import urls as flags_urls
+
 from main import urls as main_urls
 
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^flags/', include(flags_urls, namespace='flags')),
     url(r'', include(main_urls)),
 ]
 
