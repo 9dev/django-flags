cca97347153395a6442d62e2b22a756b8257ae17
9dev <9devmail@gmail.com>
2016-03-19 14:02:12 +0100

    Created urlpattern for creating new flag.

--- flags/urls.py
+++ flags/urls.py
@@ -1,5 +1,12 @@
 from django.conf.urls import patterns, url
 
+from flags import views
+
+
 urlpatterns = patterns('',               
-    # url(r'^$', someview, name='someview'),
+    url(
+        r'^create/(?P<app_name>[a-z0-9_]+)/(?P<model_name>[a-z0-9_]+)/(?P<pk>[0-9]+)$',
+        views.flag_create,
+        name='flag_create',
+    ),
 )
