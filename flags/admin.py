93205a889fb69811ec6f5f08aa5b6aacc274938b
9dev <9devmail@gmail.com>
2016-03-19 16:53:07 +0100

    Created approve action in admin panel.

--- flags/admin.py
+++ flags/admin.py
@@ -5,6 +5,12 @@ from .models import Approve, Flag
 
 class FlagAdmin(admin.ModelAdmin):
     list_display = ('content_object', 'creator', 'creation_date')
+    actions = ['approve']
+
+    def approve(self, request, queryset):
+        for flag in queryset:
+            Approve.objects.create(content_object=flag.content_object, creator=request.user)
+        self.message_user(request, "Successfully approved selected objects.")
 
 
 class ApproveAdmin(admin.ModelAdmin):
