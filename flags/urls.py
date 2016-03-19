c7e2c6f92d0561d0c22be21c1f2dff3997ac9bba
9dev <9devmail@gmail.com>
2016-03-19 15:06:05 +0100

    Added flag create page.

--- flags/urls.py
+++ flags/urls.py
@@ -5,8 +5,8 @@ from flags import views
 
 urlpatterns = patterns('',               
     url(
-        r'^create/(?P<app_name>[a-z0-9_]+)/(?P<model_name>[a-z0-9_]+)/(?P<pk>[0-9]+)$',
-        views.flag_create,
+        r'^create/(?P<app_label>[a-z0-9_]+)/(?P<model_name>[a-z0-9_]+)/(?P<pk>[0-9]+)$',
+        views.FlagCreateView.as_view(),
         name='flag_create',
     ),
 )
--- flags/views.py
+++ flags/views.py
@@ -1,3 +1,30 @@
+from django.apps import apps
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import Http404
+from django.views.generic import FormView
 
-def flag_create(request):
-    pass
+from .forms import FlagCreateForm
+from .models import Flag
+
+
+class FlagCreateView(FormView):
+    form_class = FlagCreateForm
+    template_name = 'flags/flag_form.html'
+    success_url = '/'
+
+    def get_form_kwargs(self):
+        try:
+            app_label = self.kwargs.pop('app_label')
+            model_name = self.kwargs.pop('model_name')
+            pk = self.kwargs.pop('pk')
+
+            cls = apps.get_model(app_label=app_label, model_name=model_name)
+            self.obj = cls.objects.get(pk=pk)
+        except (KeyError, LookupError, ObjectDoesNotExist):
+            raise Http404
+
+        return super(FlagCreateView, self).get_form_kwargs()
+
+    def form_valid(self, form):
+        Flag.objects.create(content_object=self.obj, creator=self.request.user)
+        return super(FlagCreateView, self).form_valid(form)
