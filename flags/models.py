a88cd0d85d91db0a78fb71ea15a88b54d699dc99
9dev <9devmail@gmail.com>
2016-03-19 16:53:29 +0100

    Created post_save signal for Approve model.

--- flags/models.py
+++ flags/models.py
@@ -1,6 +1,8 @@
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Flag(models.Model):
@@ -19,3 +21,8 @@ class Approve(models.Model):
 
     creator = models.ForeignKey('auth.User')
     creation_date = models.DateTimeField(auto_now_add=True)
+
+
+@receiver(post_save, sender=Approve)
+def my_callback(sender, instance, **kwargs):
+    Flag.objects.filter(object_id=instance.object_id, content_type=instance.content_type).delete()
--- fts/Flags.py
+++ fts/Flags.py
@@ -1,6 +1,12 @@
-from ._base import BaseTestCase
+from selenium.webdriver.support.ui import Select
+
+from django.contrib.auth.models import User
+
+from flags.models import Flag
 
 from main.models import Article
+
+from ._base import BaseTestCase
 
 
 class TestFlags(BaseTestCase):
@@ -40,6 +46,25 @@ class TestFlags(BaseTestCase):
             self.browser.current_url,
             '{}/accounts/login/?next=/flags/create/main/article/{}'.format(self.live_server_url, self.obj.pk)
         )
+
+    def test_can_approve_an_object(self):
+        # There is a flagged Article object.
+        Flag.objects.create(content_object=self.obj, creator=User.objects.get(pk=1))
+
+        # Florence logs in as an admin.
+        self.login_as_admin()
+
+        # She hits flags admin panel.
+        self.get('/admin/flags/flag')
+
+        # She approves an Article object.
+        self.get_by_id('action-toggle').click()
+        select = Select(self.browser.find_element_by_css_selector('select[name="action"]'))
+        select.select_by_visible_text('Approve')
+        self.browser.find_element_by_css_selector('button[name="index"]').click()
+
+        # She confirms that existing flag for an object disappeared.
+        self.assertIn('0 flags', self.get_text())
 
     def test_cannot_flag_object_if_approved(self):
         self.fail()
