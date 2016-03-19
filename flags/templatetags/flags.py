ee19772e944dea7e3c62f2bc2af32c11bb5dd014
9dev <9devmail@gmail.com>
2016-03-19 15:09:06 +0100

    Fixed a bug in flag_create_url templatetag.

--- flags/templatetags/flags.py
+++ flags/templatetags/flags.py
@@ -8,7 +8,7 @@ register = template.Library()
 @register.simple_tag
 def flag_create_url(obj):
     return reverse('flags:flag_create', kwargs={
-        'app_name': obj._meta.app_label,
+        'app_label': obj._meta.app_label,
         'model_name': obj._meta.object_name.lower(),
         'pk': obj.pk
     })
