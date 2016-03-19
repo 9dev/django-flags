f571b6667893a608621e4ba3c6c6f37b813fb431
9dev <9devmail@gmail.com>
2016-03-19 15:23:22 +0100

    Added __str__ method to Article model.

--- demo/main/models.py
+++ demo/main/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=100)
     content = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return '<Article id={}>'.format(self.pk)
