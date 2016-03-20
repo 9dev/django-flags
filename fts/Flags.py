893c61be91f4c3b687b05e699e1a8d58d1f375b8
9dev <9devmail@gmail.com>
2016-03-20 10:02:52 +0100

    Moved test Article object to a fixture.

--- fts/Flags.py
+++ fts/Flags.py
@@ -12,16 +12,12 @@ from ._base import BaseTestCase
 
 class TestFlags(BaseTestCase):
 
-    def setUp(self):
-        super(TestFlags, self).setUp()
-        self.obj = Article.objects.create(title='My first article', content='Some content')
-
     def test_registered_user_can_flag_an_object(self):
         # Florence logs in as an admin.
         self.login_as_admin()
 
         # She hits a detail page for an Article object.
-        self.get('/article/{}'.format(self.obj.pk))
+        self.get('/article/1')
 
         # She clicks on a link to flag the object.
         self.browser.find_element_by_partial_link_text('Report abuse').click()
@@ -33,11 +29,11 @@ class TestFlags(BaseTestCase):
         self.get('/admin/flags/flag')
 
         # She sees a new flag object.
-        self.assertIn('<Article id={}>'.format(self.obj.pk), self.get_text())
+        self.assertIn('<Article id=1>', self.get_text())
 
     def test_unregistered_user_cannot_flag_an_object(self):
         # Florence hits a detail page for an Article object.
-        self.get('/article/{}'.format(self.obj.pk))
+        self.get('/article/1')
 
         # She clicks on a link to flag the object.
         self.browser.find_element_by_partial_link_text('Report abuse').click()
@@ -45,12 +41,12 @@ class TestFlags(BaseTestCase):
         # She is asked to log in first.
         self.assertEqual(
             self.browser.current_url,
-            '{}/accounts/login/?next=/flags/create/main/article/{}'.format(self.live_server_url, self.obj.pk)
+            '{}/accounts/login/?next=/flags/create/main/article/1'.format(self.live_server_url)
         )
 
     def test_can_approve_an_object_in_admin(self):
         # There is a flagged Article object.
-        Flag.objects.create(content_object=self.obj, creator=User.objects.get(pk=1))
+        Flag.objects.create(content_object=Article.objects.get(pk=1), creator=User.objects.get(pk=1))
 
         # Florence logs in as an admin.
         self.login_as_admin()
@@ -69,7 +65,7 @@ class TestFlags(BaseTestCase):
 
     def test_can_delete_an_object_in_admin(self):
         # There is a flagged Article object.
-        Flag.objects.create(content_object=self.obj, creator=User.objects.get(pk=1))
+        Flag.objects.create(content_object=Article.objects.get(pk=1), creator=User.objects.get(pk=1))
 
         # Florence logs in as an admin.
         self.login_as_admin()
@@ -94,13 +90,13 @@ class TestFlags(BaseTestCase):
 
     def test_cannot_flag_object_if_approved(self):
         # There is an approved object.
-        Approve.objects.create(content_object=self.obj, creator=User.objects.get(pk=1))
+        Approve.objects.create(content_object=Article.objects.get(pk=1), creator=User.objects.get(pk=1))
 
         # Florence logs in as an admin.
         self.login_as_admin()
 
         # She hits a detail page for an Article object.
-        self.get('/article/{}'.format(self.obj.pk))
+        self.get('/article/1')
 
         # She clicks on a link to flag the object.
         self.browser.find_element_by_partial_link_text('Report abuse').click()
@@ -117,14 +113,15 @@ class TestFlags(BaseTestCase):
     @override_settings(FLAGS_THRESHOLD=3)
     def test_object_is_removed_after_hitting_flag_threshold_if_threshold_set(self):
         # There is an object with two flags.
-        Flag.objects.create(content_object=self.obj, creator=User.objects.get(pk=2))
-        Flag.objects.create(content_object=self.obj, creator=User.objects.get(pk=3))
+        obj = Article.objects.get(pk=1)
+        Flag.objects.create(content_object=obj, creator=User.objects.get(pk=2))
+        Flag.objects.create(content_object=obj, creator=User.objects.get(pk=3))
 
         # Florence logs in as an admin.
         self.login_as_admin()
 
         # She hits a detail page for an Article object.
-        self.get('/article/{}'.format(self.obj.pk))
+        self.get('/article/1')
 
         # She clicks on a link to flag the object.
         self.browser.find_element_by_partial_link_text('Report abuse').click()
@@ -149,7 +146,7 @@ class TestFlags(BaseTestCase):
         self.login_as_admin()
 
         # She hits a detail page for an Article object.
-        self.get('/article/{}'.format(self.obj.pk))
+        self.get('/article/1')
 
         # She clicks on a link to flag the object.
         self.browser.find_element_by_partial_link_text('Report abuse').click()
@@ -158,7 +155,7 @@ class TestFlags(BaseTestCase):
         self.submit()
 
         # She repeats that procedure.
-        self.get('/article/{}'.format(self.obj.pk))
+        self.get('/article/1')
         self.browser.find_element_by_partial_link_text('Report abuse').click()
         self.submit()
 
